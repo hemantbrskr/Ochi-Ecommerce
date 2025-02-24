dcab2d2afbaaa6ee8bfc572e72d9ba2da8867809
hemant <hunnyybarasker01@gmail.com>
2025-02-24 17:14:12 +0530

    Working Project

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import LandingPage from './components/LandingPage'
 import { Marquee } from './components/Marquee'
- import { About } from './components/About'
+import { About } from './components/About'
+import { Eyes } from './components/Eyes'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <LandingPage/>
     <Marquee/>
     <About/>
+    <Eyes/>
     </div>
    
   )
